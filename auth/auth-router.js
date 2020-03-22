0b6b8f97af9e7acb8906b0e6e02dcec6c4ffa2c4
unknown <taveras574@gmail.com>
2020-03-22 19:47:55 -0400

    Register and login endpoints are now functional

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,51 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const db = require("../database/dbConfig");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    // implement registration
+    if (!username || !password) {
+      return res.status(400).json({
+        message: "All fields are required!"
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db("users").insert({
+      username: username,
+      password: hashedPassword
+    });
+
+    res.json({
+      message: "success"
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", async (req, res, next) => {
   // implement login
+  try {
+    const { username, password } = req.body;
+    const user = await db("users").where("username", username).first();
+    const passwordMatches = await bcrypt.compare(password, user.password)
+    if(!user || !passwordMatches){
+      return res.status(401).json({
+        message: "Invalid credentials!"
+      })
+    }
+
+    const token = jwt.sign({id: user.ud}, 'shhhhhhhh');
+    res.json({
+      token: token
+    })
+
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 3300;
+
+
+
+server.use((error, req, res, next)=>{
+  console.log(error);
+  res.status(500).json({
+    message: "There was an internal server error!"
+  })
+})
+
+
+
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
